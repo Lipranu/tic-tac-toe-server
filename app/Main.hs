27c017f8e297bebbc3b823a1fbd05bc07a1dbdbd
Igor Belousov <lipranu@gmail.com>
2024-02-05 06:13:48 +0700

    Added launch code in Main.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import qualified Server (someFunc)
+import Control.Concurrent.Async (concurrently_)
+import Control.Concurrent.STM.TVar (newTVarIO)
+import Data.HashMap.Strict (empty)
+import Network.Wai.Handler.Warp (run)
 
+import qualified Server
+import qualified SessionManager
+
+--------------------------------------------------------------------------------
+
+-- | Запускает приложение и менеджер сессий.
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  Server.someFunc
+  initialState <- newTVarIO empty
+  let runApp = run port $ Server.app initialState
+      runManager = SessionManager.run initialState
+  concurrently_ runApp runManager
+  where port = 8080
